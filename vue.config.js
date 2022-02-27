9dd492a8a93f7941481fd0cacf89404ca93fa069
pycyyaxyy <pycyyaxyy@gmail.com>
2022-02-28 01:01:22 +0800

    修改了vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,42 @@
-const { defineConfig } = require("@vue/cli-service")
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-})
+
+  //配置方式1-1 使用cli提供的属性
+  outputDir: "./build",
+  // publicPath: './',
+  //配置方式1-2 和webpack属性完全一致 会进行合并
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://152.136.185.210:4000",
+        pathRewrite: {
+          "^/api": "",
+        },
+        changeOrigin: true,
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        views: "@/views",
+        components: "@/components",
+      },
+    },
+  },
+  //配置方式2：通过configureWebpack修改webpack的配置：可以是一个函数，会接收一个config，可以通过config来修改配置；
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     views: '@/views'
+  //   }
+  // }
+
+  //配置方式3：通过chainWebpack修改webpack的配置：
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('views', '@/views')
+  // }
+});
